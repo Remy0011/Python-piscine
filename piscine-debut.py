68b6a1e0cdef29bd5b1cfe403024fe84962e6e61
Remy0011 <remy.deiber1999@gmail.com>
2024-11-25 10:42:57 +0100

    Modif listeapend

--- piscine-debut.py
+++ piscine-debut.py
@@ -1,15 +1,19 @@
 liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
 commande = ''
 
+def cmd_ajout(liste):
+    """Ajoute un évènement à la liste"""
+    a = input("Qui nage?")
+    b = input("quelle nage ?")
+    c = input("combien de longueur ?")
+    liste.append ((a,b,c))
+
 isAlive = True
 while isAlive:
     commande = input("Que faut-il faire ? ")
 
     if commande == 'ajout':
-        a = input("Qui nage ? ")
-        b = input("quelle nage ? ")
-        c = input("combien de longueur ? ")
-        liste.append((a,b,c))
+        cmd_ajout(liste)
         continue
    
     if commande == 'liste':
@@ -18,7 +22,9 @@ while isAlive:
         continue
 
     if commande == 'exit':
-        isAlive = False
+        tmp = input ("En êtes-vous sûr ? (o)ui:(n)on")
+        if tmp == 'o':
+            isAlive = False
         continue
 
     print(f"Commande{commande}inconnue")
